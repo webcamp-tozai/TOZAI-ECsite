e661646eb5249ab0f5605ae383a79249cbeb8d45
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-12 12:23:42 +0000

    add controller view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,18 @@
 class ContactsController < ApplicationController
+	def index
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	private
+	def contact_params
+		params.require(:contact).permit(:contact_status_id,:name,:title,:email,:message)
+	end
 end
