76667502e9e162a5ec37675fbb29f8f838b4a430
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-12 07:40:48 +0000

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  resources :items, except: [:destroy] do
+
+
+
+
+
+
+
+
+
+
+
+
+
   	resource:item_reviews,only:[:index,:create,:new,:edit,:update,:destroy]
   end
 end
