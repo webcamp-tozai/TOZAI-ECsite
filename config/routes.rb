f557f0e41621c629aaedd8cc2e3d0a015c454d74
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-12 05:55:34 +0000

    view作成 routes itemへネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :items, except: [:destroy]
+  resources :items, except: [:destroy] do
+  	resource:item_reviews,only:[:index,:create,:new,:edit,:update,:destroy]
+  end
 end
