fcb82c6e324d7492be0c6799b3480b49abcd692c
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-12 18:07:22 +0900

    細かい修正

--- db/migrate/20180612090143_create_item_reviews.rb
+++ db/migrate/20180612090143_create_item_reviews.rb
@@ -5,7 +5,7 @@ class CreateItemReviews < ActiveRecord::Migration[5.1]
       # NOT NULL => false
       # index指定 => true
       t.integer     :item_id, null: false, index: true
-      t.integer     :user_if, null: false, index: true
+      t.integer     :user_id, null: false, index: true
       t.text        :review,  null: false, index: true
       t.timestamps
     end
