fd8b758e280eb254b018ce7600b7c1ab3a955ca5
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-12 17:48:30 +0000

    Artistモデルの追加

---
+++ db/migrate/20180612174541_create_artists.rb
@@ -0,0 +1,13 @@
+class CreateArtists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :artists do |t|
+      # 追加
+      # NOT NULL => false
+      # index指定 => true
+      t.integer     :track_id,  null: false, index: true
+      t.string      :name,      null: false, index: true
+      t.string      :name_kana, null: false, index: true
+      t.timestamps
+    end
+  end
+end
