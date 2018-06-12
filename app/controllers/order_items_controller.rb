fcd3922d20c08c1aebf7fd3b240a847badb187e9
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-12 12:09:12 +0000

    add controller,view

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,12 @@
 class OrderItemsController < ApplicationController
+	def index
+	end
+
+	def create
+	end
+
+	private
+	def order_item_params
+		params.require(:order_item).permit(:item_id,:item_count,:total_price_without_tax)
+	end
 end
