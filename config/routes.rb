3ad8258924acd1d5b6682dc6ee6aff13b7b7c1e2
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-12 07:44:04 +0000

    コンフリクト解消2

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+  }
+  
+  resources :items, except: [:destroy] do
   	resource:item_reviews,only:[:index,:create,:new,:edit,:update,:destroy]
   end
 end
