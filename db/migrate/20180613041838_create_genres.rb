fa0eb330d139bda54405468e28ca3b875f02dfed
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-13 04:20:16 +0000

    Genreモデルの追加

---
+++ db/migrate/20180613041838_create_genres.rb
@@ -0,0 +1,13 @@
+class CreateGenres < ActiveRecord::Migration[5.1]
+  def change
+    create_table :genres do |t|
+      # 追加
+      # NOT NULL => false
+      # index指定 => true
+      t.string      :genre_english, null: false, index: true
+      t.string      :genre_kana,    null: false, index: true
+      
+      t.timestamps
+    end
+  end
+end
