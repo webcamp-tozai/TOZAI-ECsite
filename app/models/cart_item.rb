8b46a272c2e3175a985dc71ac9fdc775fcf84208
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-13 04:27:47 +0000

    CartItemモデルのバリデーション追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,8 @@
 class CartItem < ApplicationRecord
+  validates :item_id,     presence: true
+  validates :user_id,     presence: true
+  validates :item_count,  presence: true,
+    length: { minimum: 0 },
+    format: { with: /\A\d\z/, # 0以上の半角数字のみ
+              message: "半角数字で入力して下さい。"  }
 end
