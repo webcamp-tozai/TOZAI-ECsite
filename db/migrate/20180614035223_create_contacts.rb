23b420552f08eab96ee7ef197647624bb1d0ff1a
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-14 03:54:54 +0000

    Contactモデルの追加

---
+++ db/migrate/20180614035223_create_contacts.rb
@@ -0,0 +1,16 @@
+class CreateContacts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :contacts do |t|
+      # 追加
+      # NOT NULL => false
+      # index指定 => true
+      t.integer   :contact_status_id, null: false, index: true
+      t.string    :name, null: false, index: true
+      t.string    :title, null: false
+      t.string    :email, null: false, index: true
+      t.text      :message, null: false
+      
+      t.timestamps
+    end
+  end
+end
