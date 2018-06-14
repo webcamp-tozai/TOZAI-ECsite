40fd1f99c7ea9591c7214e417479ebb9a95fb61a
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-14 11:37:55 +0000

    modified user sign_up view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys:[:famili_name_kanji,:given_name_kanji])
+  	devise_parameter_sanitizer.permit(:sing_in, keys:[:famili_name_kanji,:given_name_kanji])
+  end
 end
