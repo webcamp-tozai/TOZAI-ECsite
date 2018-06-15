4864198994080fef66dc7cdcbb5f87c89dfadac1
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-15 12:38:49 +0000

    seed_fu動作確認終了

---
+++ db/fixtures/development/genre.rb
@@ -0,0 +1,11 @@
+Genre.seed(:id) do |s|
+  s.id = 1
+  s.genre_english = "Rock"
+  s.genre_kana = "ロック"
+end
+
+Genre.seed(:id) do |s|
+  s.id = 2
+  s.genre_english = "Jazz"
+  s.genre_kana = "ジャズ"
+end
