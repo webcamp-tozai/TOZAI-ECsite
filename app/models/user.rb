c202d939a9b4f1fb4785f76ce4a2414e6777405d
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-15 11:51:19 +0000

    Userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,41 +5,61 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   
   # Validatoin
-  validates :password,
-  	length: {
-  		in: 6..20,
-  		message: "6〜20文字までのパスワードを入力してください"
-  		}
-  validates :family_name_kanji,
+  validates :family_name_kanji, presence: true,
   	format: {
   		with: /\A[ぁ-んァ-ン一-龥a-zA-Z]+\z/,
   		message: "全角の日本語か半角の英語で入力して下さい"
-  		}
-  validates :given_name_kanji,
+  	}
+  	
+  validates :given_name_kanji,  presence: true,
   	format: {
   		with: /\A[ぁ-んァ-ン一-龥a-zA-Z]+\z/,
   		message: "全角の日本語か半角の英語で入力して下さい"
-  		}
-  validates :family_name_kana,
+  	}
+  	
+  validates :family_name_kana,  presence: true,
 		format: {
 			with: /\A[ァ-ンー－]+\z/,
 			message: "全角のカタカナで入力して下さい"
-			}
-  validates :given_name_kana,
+		}
+		
+  validates :given_name_kana,   presence: true,
 	  format: {
 	  	with: /\A[ァ-ンー－]+\z/,
 	  	message: "全角のカタカナで入力して下さい"
-	  	}
-  validates :phone_number,
+	  }
+	  
+  validates :phone_number,      presence: true,
 		format: {
 			with: /\A\d{10}\z|\A\d{11}\z/,
 			message: "ハイフン無しの半角数字のみで入力してください"
-			}
-  validates :post_code,
+		}
+		
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email,             	presence: true, uniqueness: true,
+    format: { 
+      with: VALID_EMAIL_REGEX, 
+      message: "記入内容を確認して下さい。"
+    }
+              
+	validates :password,          presence: true,
+  	length: {
+  		in: 6..20,
+  		message: "6〜20文字までのパスワードを入力してください"
+  	}
+  	
+  validates :post_code,         presence: true,
     format: {
 			with: /\A\d{7}\z/,
 			message: "ハイフン無しの半角数字のみで入力してください"
-			}
+		}
+		
+	validates :address,           presence: true
+	validates :member_status,     presence: true,
+	  format: {
+			with: /\A\d\z/, # 0 => 入会、1 => 退会済み、2 => 強制退会
+			message: "半角数字で入力して下さい。"
+		}
   					
   # Association
   has_one :cart_item
