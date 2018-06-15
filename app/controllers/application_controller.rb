2c1ac614514fcb8d4e26c4de971e573d38341cec
yasmaimo <yasmaimo0141@gmail.com>
2018-06-15 09:04:43 +0000

    管理者サイドバー機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def after_sign_in_path_for(resource)
-		root_path
+		if admin_signed_in?
+			admins_top_path
+		else
+			root_path
+		end
 	end
 	def after_sign_out_path_for(resource)
 		root_path
