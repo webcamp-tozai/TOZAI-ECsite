7600b1e45d7814282c5039ddf523c3aa93305fa6
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-06-16 07:16:31 +0000

    emailのユニーク削除

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,15 +3,14 @@ class Contact < ApplicationRecord
   validates :contact_status_id, presence: true
   validates :name,              presence: true
   validates :title,             presence: true
-  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,             presence: true, 
-                                uniqueness: true,
     format: { with: VALID_EMAIL_REGEX, 
               message: "記入内容を確認して下さい。"  }
-              
+
   validates :message,           presence: true
-  
+
   # Association
   belongs_to :contact_status # contact_status has_many :contacts
 end
