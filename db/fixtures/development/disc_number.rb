99c06daaf554d60aa77d14e7d39627eea928c007
yasmaimo <yasmaimo0141@gmail.com>
2018-06-16 16:39:40 +0000

    disc numberのカラム追加、アソシエーション修正 db更新

--- db/fixtures/development/disc_number.rb
+++ db/fixtures/development/disc_number.rb
@@ -1,5 +1,23 @@
+#商品1 (在庫残り100個の4曲入りシングル)
 DiscNumber.seed(:id,
-  { id: 1, number: 1 },
-  { id: 2, number: 2 },
-  { id: 3, number: 3 }
+  { id: 1, item_id: 1, number: 1 }
+)
+
+#商品2 (在庫残り20個のディスク2枚組、各ディスク5曲入りのアルバム)
+DiscNumber.seed(:id,
+  { id: 2, item_id: 2, number: 1 },
+  { id: 3, item_id: 2, number: 2 }
+)
+
+#商品3 (在庫切れの4曲入りレコード)
+DiscNumber.seed(:id,
+  { id: 4, item_id: 3, number: 1 }
+)
+
+#商品4 (取扱終了済のディスク4枚組、各ディスク1曲入りのアルバム)
+DiscNumber.seed(:id,
+  { id: 5, item_id: 4, number: 1 },
+  { id: 6, item_id: 4, number: 2 },
+  { id: 7, item_id: 4, number: 3 },
+  { id: 8, item_id: 4, number: 4 }
 )
