6531884b561224c791846e7c202fd3c709e9125d
yasmaimo <yasmaimo0141@gmail.com>
2018-06-16 09:52:53 +0000

    add-artist-test-data

---
+++ db/fixtures/development/artist.rb
@@ -0,0 +1,6 @@
+Artist.seed(:id,
+  { id: 1, name: "Nickelback", name_kana: "ニッケルバック" },
+  { id: 2, name: "スピッツ", name_kana: "スピッツ" },
+  { id: 3, name: "秦基博", name_kana: "ハタモトヒロ" },
+  { id: 4, name: "中島みゆき", name_kana: "ナカジマミユキ" }
+)
