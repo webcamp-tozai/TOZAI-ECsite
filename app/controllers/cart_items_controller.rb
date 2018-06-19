ece37c0e0b76f72a6d4a6037db58be51b2d79a79
yasmaimo <yasmaimo0141@gmail.com>
2018-06-19 07:55:27 +0000

    数量変更機能実装

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,8 +1,9 @@
 class CartItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_cart_item, only: %w(show edit update destroy)
 
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find(current_user.id)
     @cart_item = current_user.cart_items.all
     if params[:id].present?
       set_cart_item
@@ -15,16 +16,13 @@ class CartItemsController < ApplicationController
     item = Item.find(params[:item_id])
     cart_item = item.cart_items.new(cart_item_params)
     cart_item.user_id = current_user.id
-    if item.stock - cart_item.item_count >= 0 || item.is_deleted == true
-      #cart_item = CartItem.find_or_initialize_by(item_id: item.id, user_id: current_user.id)
-      if cart_item.new_record?
-        cart_item.save
-      #else
-        #cart_item.update_attributes(item_count: cart_item.item_count)
-      end
-      redirect_to user_cart_items_path(cart_item.user_id)
+    if item.stock - cart_item.item_count >= 0
+      cart_item.save
+      flash[:cart_item_created] = "カートに商品を追加しました"
+      redirect_to user_cart_items_path(current_user)
     else
       redirect_to item_path(item)
+      flash[:cart_item_create_faled] = "入力した数量が在庫数を上回っています。残りの在庫数は#{item.stock}個です。"
     end
   end
 
@@ -32,27 +30,27 @@ class CartItemsController < ApplicationController
     cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
     redirect_to user_cart_items_path(cart_item)
-    flash[:updated] = "変更を保存しました"
+    flash[:cart_item_updated] = "変更を保存しました"
   end
 
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
     redirect_to user_cart_items_path(cart_item.user_id)
-    flash[:destroyed] = "カートから商品を削除しました"
+    flash[:cart_item_destroyed] = "カートから商品を削除しました"
   end
 
   def all_destroy
     cart_item = current_user.cart_items.all
     cart_item.destroy_all
     redirect_to user_cart_items_path(current_user)
-    flash[:destroyed] = "カートから商品を削除しました"
+    flash[:cart_item_all_destroyed] = "カートを空にしました"
   end
 
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :user_id, :item_count, :id)
+    params.require(:cart_item).permit(:item_id, :user_id, :item_count)
   end
 
   def set_cart_item
