ced719a10fbcd7cb0c9be40146fc05b54059c569
yasmaimo <yasmaimo0141@gmail.com>
2018-06-21 10:31:43 +0000

    購入機能実装

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,10 +3,13 @@ class OrderItemsController < ApplicationController
 	end
 
 	def create
+		order_item = OrderItem.new(oreder_item_params)
+		order_item.save
+    redirect_to orders_path
 	end
 
 	private
 	def order_item_params
-		params.require(:order_item).permit(:item_id,:item_count,:total_price_without_tax)
+		params.require(:order_item).permit(:id, :order_id, :item_id, :item_count, :total_price_without_tax)
 	end
 end
