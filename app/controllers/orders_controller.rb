8ae30eeab341cd4c54c037317ca8eddafab865c2
yasmaimo <yasmaimo0141@gmail.com>
2018-06-22 08:41:43 +0000

    購入後の在庫を減らす機能、購入時に在庫切れor販売終了になっていた場合の処理実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,5 @@
-
 class OrdersController < ApplicationController
 	before_action :authenticate_user_or_admin, except: [:index]
-	before_action :authenticate_current_user, except: [:index]
 
 	def index
 		if user_signed_in?
@@ -80,9 +78,43 @@ class OrdersController < ApplicationController
 	end
 
 	def create
-    order = Order.new(order_params)
-    order.save
-  	redirect_to orders_path
+  	order = Order.new(order_params)
+  	cart_items = current_user.cart_items
+  	cart_items.each do |cart_item|
+  		item = Item.find(cart_item.item_id)
+  		# 在庫がゼロになった時の検証用
+  		# item.stock = 0
+  		# 販売終了になった時の検証用
+  		item.is_deleted = true
+    	if item.stock == 0
+    		cart_item.destroy
+    		redirect_to new_order_path
+    		flash[:cart_item_force_destroyed] = "#{item.title}は在庫切れになったため、カートから削除しました。"
+    		return
+    	elsif item.is_deleted == true
+    		cart_item.destroy
+    		redirect_to new_order_path
+    		flash[:item_is_deleted] = "#{item.title}は販売終了となったため、カートから削除しました。"
+    		return
+    	elsif item.stock - cart_item.item_count < 0
+    		difference_negative = item.stock - cart_item.item_count
+    		difference = 0 - difference_negative
+    		item_count = cart_item.item_count - difference
+    		cart_item.update(item_count: item_count)
+    		redirect_to new_order_path
+    		flash[:order_created_faled] = "#{item.title}の数量が在庫数を上回っています。こちらの商品は最大で#{cart_item.item_count}個購入できます。"
+    		flash[:cart_item_updated] = "#{item.title}の数量を#{cart_item.item_count}個に変更しました"
+    		return
+    	end
+    end
+	  order.save
+	  cart_items.each do |cart_item|
+			item = Item.find(cart_item.item_id)
+	  	stock = item.stock - cart_item.item_count
+	  	item.update(stock: stock)
+  	end
+		cart_items.destroy_all
+ 		redirect_to orders_path
 	end
 
 	def new
@@ -114,67 +146,8 @@ class OrdersController < ApplicationController
     end
   end
 
-  def authenticate_current_user
-    if user_signed_in?
-      if @user.id != current_user.id
-        redirect_to items_path
-      end
-    end
-  end
-
 	def order_params
 		params.require(:order).permit(:user_id, :address_id, :payment_id, :status, :total_count, :total_price_without_tax, :total_price, order_items_attributes: [:order_id, :item_id, :item_count, :total_price_without_tax])
 	end
 
-	private
-
-  def authenticate_user_or_admin
-    if admin_signed_in? || user_signed_in?
-    else
-      redirect_to items_path
-    end
-  end
-
-  def authenticate_current_user
-    if user_signed_in?
-      if @user.id != current_user.id
-        redirect_to items_path
-      end
-    end
-  end
-
-=begin
-
-ステータス絞り込みのコーディングがダサいのであとで簡略化したい
-  def find_orders
-  	if admin_signed_in?
-  		if action_name = "index"
-  			@status_name = "全て"
-  			@orders = Order.all
-  		elsif action_name = "orders_status1"
-				@status_name = "注文受付"
-				@orders = Order.where(status: "注文受付")
-			elsif action_name = "orders_status2"
-				@status_name = "発送準備中"
-				@orders = Order.where(status: "発送準備中")
-			elsif action_name = "orders_status3"
-				@status_name = "発送済"
-				@orders = Order.where(status: "発送済")
-			elsif action_name = "orders_status4"
-				@status_name = "配達完了"
-				@orders = Order.where(status: "配達完了")
-			end
-			@status1 = Order.where(status: "注文受付")
-			@status2 = Order.where(status: "発送準備中")
-			@status3 = Order.where(status: "発送済")
-			@status4 = Order.where(status: "配達完了")
-			@order_items = OrderItem.where(order_id: @orders)
-		end
-  end
-
-=end
-
-	def order_params
-		params.require(:order).permit(:user_id, :address_id, :payment_id, :status, :total_count, :total_price_without_tax, :total_price, order_items_attributes: [:order_id, :item_id, :item_count, :total_price_without_tax])
-	end
 end
