f98553283d011a0551e369b781e8e8ce4b10cde6
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-22 11:01:55 +0000

    item登録、track追加、track削除実装OK

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,21 +6,21 @@ class Item < ApplicationRecord
   validates :artist_id,         presence: true
   validates :title,             presence: true
   #validates :image_id,          presence: true
-  
+
   validates :stock,             presence: true,
     numericality: { greater_than_or_equal_to: 0 }, #0以上
     format: { with: /\A\d+\z/, # 半角数字のみ
               message: "0以上の半角数字のみ。"  }
-              
+
   validates :price_without_tax, presence: true,
     numericality: { greater_than_or_equal_to: 0 }, #0以上
     format: { with: /\A\d+\z/, # 半角数字のみ
               message: "0以上の半角数字のみ。"  }
-              
+
   validates :content_type,      presence: true
-  
+
   validates :is_deleted,        inclusion: { in: [true, false] }
-                                
+
   # Association
   belongs_to :label #label has_many :items
   belongs_to :genre #genre has_many :items
