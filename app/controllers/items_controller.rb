1b7a7b6ec1d9aee4ba96175133d445ac720490e8
kazuya-hachisuka <kzy8sk@gmail.com>
2018-06-24 09:09:26 +0000

    genre edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_genre, only: [:index, :show, :genre_index, :artist_index]
 
   PER_ITEM = 12
-  
+
   def index
     @items = Item.page(params[:page]).reverse_order.per(PER_ITEM)
   end
